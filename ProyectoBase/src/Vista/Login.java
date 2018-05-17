22b9e502fcdc7c689e8ce8148132ce276a4d5d22
ALUMNADO\miamme <xeviusthelombax@gmail.com>
2018-05-17 16:52:43 +0200

    Cambio la clase login por una mejorada

--- ProyectoBase/src/Vista/Login.java
+++ ProyectoBase/src/Vista/Login.java
@@ -6,30 +6,30 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Component;
 import javax.swing.Box;
-import javax.swing.JSeparator;
+import javax.swing.JPasswordField;
 import java.awt.Color;
 import javax.swing.JButton;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
 
 public class Login extends JFrame {
 
-	private JPanel contLogo;
-	private JTextField campEmail;
-	private JTextField campoPassword;
-	private JLabel logo;
-	private JLabel lblEmail;
-	private JLabel lblPassword;
-	private JButton btnIniciarSesion;
-	private JButton btnRecuperarContr;
-	private JButton btnNuevoUsuario;
+	private JPanel contenedorPrincipal;
+	private JTextField campoNombreUsuario;
+	private JTextField campoUsuario;
 
 	/**
 	 * Launch the application.
@@ -51,145 +51,158 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
-		setTitle("Nombre generico de Aplicacion #4578");
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 900, 500);
-		contLogo = new JPanel();
-		contLogo.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contLogo.setLayout(new BorderLayout(0, 0));
-		setContentPane(contLogo);
+		setBounds(100, 100, 888, 600);
 		
-		logo = new JLabel("");
-		logo.setHorizontalAlignment(SwingConstants.CENTER);
-		logo.setIcon(new ImageIcon("E:\\Florida\\PI\\TEST.png"));
-		contLogo.add(logo, BorderLayout.NORTH);
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
 		
-		JPanel contCampos = new JPanel();
-		contLogo.add(contCampos, BorderLayout.CENTER);
-		GridBagLayout gbl_contCampos = new GridBagLayout();
-		gbl_contCampos.columnWidths = new int[]{186, 254, 254, 0};
-		gbl_contCampos.rowHeights = new int[]{36, 14, 20, 20, 14, 20, 20, 2, 23, 20, 23, 2, 0};
-		gbl_contCampos.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_contCampos.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		contCampos.setLayout(gbl_contCampos);
+		JMenu mnIdioma = new JMenu("Idioma");
+		menuBar.add(mnIdioma);
 		
-		lblEmail = new JLabel("Email");
-		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblEmail = new GridBagConstraints();
-		gbc_lblEmail.anchor = GridBagConstraints.NORTH;
-		gbc_lblEmail.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblEmail.insets = new Insets(0, 0, 5, 0);
-		gbc_lblEmail.gridwidth = 2;
-		gbc_lblEmail.gridx = 1;
-		gbc_lblEmail.gridy = 1;
-		contCampos.add(lblEmail, gbc_lblEmail);
+		JMenuItem mntmEspaol = new JMenuItem("Espa\u00F1ol");
+		mntmEspaol.setIcon(new ImageIcon(Login.class.getResource("/assets/esp.png")));
+		mnIdioma.add(mntmEspaol);
 		
-		campEmail = new JTextField();
-		GridBagConstraints gbc_campEmail = new GridBagConstraints();
-		gbc_campEmail.anchor = GridBagConstraints.NORTH;
-		gbc_campEmail.fill = GridBagConstraints.HORIZONTAL;
-		gbc_campEmail.insets = new Insets(0, 0, 5, 0);
-		gbc_campEmail.gridwidth = 2;
-		gbc_campEmail.gridx = 1;
-		gbc_campEmail.gridy = 2;
-		contCampos.add(campEmail, gbc_campEmail);
-		campEmail.setColumns(10);
+		JMenuItem mntmIngles = new JMenuItem("Ingles");
+		mntmIngles.setIcon(new ImageIcon(Login.class.getResource("/assets/eng.png")));
+		mnIdioma.add(mntmIngles);
+		contenedorPrincipal = new JPanel();
+		contenedorPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contenedorPrincipal);
 		
-		Component espacioEmailPassword = Box.createVerticalStrut(20);
-		GridBagConstraints gbc_espacioEmailPassword = new GridBagConstraints();
-		gbc_espacioEmailPassword.anchor = GridBagConstraints.NORTH;
-		gbc_espacioEmailPassword.insets = new Insets(0, 0, 5, 5);
-		gbc_espacioEmailPassword.gridx = 1;
-		gbc_espacioEmailPassword.gridy = 3;
-		contCampos.add(espacioEmailPassword, gbc_espacioEmailPassword);
+		JPanel contenedorLogo = new JPanel();
 		
-		lblPassword = new JLabel("Contrase\u00F1a");
-		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
-		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
-		gbc_lblPassword.anchor = GridBagConstraints.NORTH;
-		gbc_lblPassword.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblPassword.insets = new Insets(0, 0, 5, 0);
-		gbc_lblPassword.gridwidth = 2;
-		gbc_lblPassword.gridx = 1;
-		gbc_lblPassword.gridy = 4;
-		contCampos.add(lblPassword, gbc_lblPassword);
+		JSeparator separator = new JSeparator();
+		separator.setForeground(Color.BLACK);
 		
-		campoPassword = new JTextField();
-		GridBagConstraints gbc_campoPassword = new GridBagConstraints();
-		gbc_campoPassword.anchor = GridBagConstraints.NORTH;
-		gbc_campoPassword.fill = GridBagConstraints.HORIZONTAL;
-		gbc_campoPassword.insets = new Insets(0, 0, 5, 0);
-		gbc_campoPassword.gridwidth = 2;
-		gbc_campoPassword.gridx = 1;
-		gbc_campoPassword.gridy = 5;
-		contCampos.add(campoPassword, gbc_campoPassword);
-		campoPassword.setColumns(10);
+		JPanel contenedorCampos = new JPanel();
 		
-		Component espacioContrSeparador = Box.createVerticalStrut(20);
-		GridBagConstraints gbc_espacioContrSeparador = new GridBagConstraints();
-		gbc_espacioContrSeparador.anchor = GridBagConstraints.NORTH;
-		gbc_espacioContrSeparador.insets = new Insets(0, 0, 5, 5);
-		gbc_espacioContrSeparador.gridx = 1;
-		gbc_espacioContrSeparador.gridy = 6;
-		contCampos.add(espacioContrSeparador, gbc_espacioContrSeparador);
+		JSeparator separador1 = new JSeparator();
+		separador1.setForeground(Color.BLACK);
 		
-		JSeparator separadorCamposBotones = new JSeparator();
-		separadorCamposBotones.setForeground(Color.BLACK);
-		GridBagConstraints gbc_separadorCamposBotones = new GridBagConstraints();
-		gbc_separadorCamposBotones.anchor = GridBagConstraints.NORTH;
-		gbc_separadorCamposBotones.fill = GridBagConstraints.HORIZONTAL;
-		gbc_separadorCamposBotones.insets = new Insets(0, 0, 5, 0);
-		gbc_separadorCamposBotones.gridwidth = 2;
-		gbc_separadorCamposBotones.gridx = 1;
-		gbc_separadorCamposBotones.gridy = 7;
-		contCampos.add(separadorCamposBotones, gbc_separadorCamposBotones);
+		JPanel contenedorBotones = new JPanel();
+		GroupLayout gl_contenedorPrincipal = new GroupLayout(contenedorPrincipal);
+		gl_contenedorPrincipal.setHorizontalGroup(
+			gl_contenedorPrincipal.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_contenedorPrincipal.createSequentialGroup()
+					.addGap(130)
+					.addGroup(gl_contenedorPrincipal.createParallelGroup(Alignment.TRAILING)
+						.addComponent(contenedorLogo, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE)
+						.addComponent(contenedorCampos, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE)
+						.addComponent(separador1, GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE)
+						.addComponent(contenedorBotones, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 590, Short.MAX_VALUE)
+						.addComponent(separator, GroupLayout.DEFAULT_SIZE, 590, Short.MAX_VALUE))
+					.addGap(142))
+		);
+		gl_contenedorPrincipal.setVerticalGroup(
+			gl_contenedorPrincipal.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contenedorPrincipal.createSequentialGroup()
+					.addGap(40)
+					.addComponent(contenedorLogo, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(separador1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(29)
+					.addComponent(contenedorCampos, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+					.addGap(30)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(31)
+					.addComponent(contenedorBotones, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(31, Short.MAX_VALUE))
+		);
+		contenedorBotones.setLayout(new GridLayout(0, 5, 0, 0));
 		
-		btnIniciarSesion = new JButton("Iniciar Sesion");
-		GridBagConstraints gbc_btnIniciarSesion = new GridBagConstraints();
-		gbc_btnIniciarSesion.anchor = GridBagConstraints.NORTH;
-		gbc_btnIniciarSesion.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnIniciarSesion.insets = new Insets(0, 0, 5, 0);
-		gbc_btnIniciarSesion.gridwidth = 2;
-		gbc_btnIniciarSesion.gridx = 1;
-		gbc_btnIniciarSesion.gridy = 8;
-		contCampos.add(btnIniciarSesion, gbc_btnIniciarSesion);
+		Component horizontalStrut_8 = Box.createHorizontalStrut(20);
+		contenedorBotones.add(horizontalStrut_8);
 		
-		Component espacioBotones = Box.createVerticalStrut(20);
-		GridBagConstraints gbc_espacioBotones = new GridBagConstraints();
-		gbc_espacioBotones.anchor = GridBagConstraints.NORTH;
-		gbc_espacioBotones.insets = new Insets(0, 0, 5, 5);
-		gbc_espacioBotones.gridx = 1;
-		gbc_espacioBotones.gridy = 9;
-		contCampos.add(espacioBotones, gbc_espacioBotones);
+		JButton botonInicioSesion = new JButton("Iniciar Sesion");
+		contenedorBotones.add(botonInicioSesion);
 		
-		btnRecuperarContr = new JButton("Recuperar Contrase\u00F1a");
-		GridBagConstraints gbc_btnRecuperarContr = new GridBagConstraints();
-		gbc_btnRecuperarContr.anchor = GridBagConstraints.NORTH;
-		gbc_btnRecuperarContr.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnRecuperarContr.insets = new Insets(0, 0, 5, 5);
-		gbc_btnRecuperarContr.gridx = 1;
-		gbc_btnRecuperarContr.gridy = 10;
-		contCampos.add(btnRecuperarContr, gbc_btnRecuperarContr);
+		Component horizontalStrut_9 = Box.createHorizontalStrut(20);
+		contenedorBotones.add(horizontalStrut_9);
 		
-		btnNuevoUsuario = new JButton("Nuevo Usuario");
-		GridBagConstraints gbc_btnNuevoUsuario = new GridBagConstraints();
-		gbc_btnNuevoUsuario.anchor = GridBagConstraints.NORTH;
-		gbc_btnNuevoUsuario.fill = GridBagConstraints.HORIZONTAL;
-		gbc_btnNuevoUsuario.insets = new Insets(0, 0, 5, 0);
-		gbc_btnNuevoUsuario.gridx = 2;
-		gbc_btnNuevoUsuario.gridy = 10;
-		contCampos.add(btnNuevoUsuario, gbc_btnNuevoUsuario);
+		JButton botonNuevoUsuario = new JButton("Nuevo Usuario");
+		contenedorBotones.add(botonNuevoUsuario);
 		
-		JSeparator separadorBotonesBorde = new JSeparator();
-		separadorBotonesBorde.setForeground(Color.BLACK);
-		GridBagConstraints gbc_separadorBotonesBorde = new GridBagConstraints();
-		gbc_separadorBotonesBorde.anchor = GridBagConstraints.NORTH;
-		gbc_separadorBotonesBorde.fill = GridBagConstraints.HORIZONTAL;
-		gbc_separadorBotonesBorde.gridwidth = 2;
-		gbc_separadorBotonesBorde.gridx = 1;
-		gbc_separadorBotonesBorde.gridy = 11;
-		contCampos.add(separadorBotonesBorde, gbc_separadorBotonesBorde);
+		Component horizontalStrut_12 = Box.createHorizontalStrut(20);
+		contenedorBotones.add(horizontalStrut_12);
+		
+		Component verticalStrut = Box.createVerticalStrut(50);
+		contenedorBotones.add(verticalStrut);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(50);
+		contenedorBotones.add(verticalStrut_1);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(50);
+		contenedorBotones.add(verticalStrut_2);
+		
+		Component verticalStrut_3 = Box.createVerticalStrut(20);
+		contenedorBotones.add(verticalStrut_3);
+		
+		Component verticalStrut_4 = Box.createVerticalStrut(20);
+		contenedorBotones.add(verticalStrut_4);
+		
+		Component horizontalStrut_10 = Box.createHorizontalStrut(20);
+		contenedorBotones.add(horizontalStrut_10);
+		
+		Component horizontalStrut_11 = Box.createHorizontalStrut(20);
+		contenedorBotones.add(horizontalStrut_11);
+		
+		JButton botonRecuperarPassword = new JButton("Recuperar Contrase\u00F1a");
+		contenedorBotones.add(botonRecuperarPassword);
+		
+		Component horizontalStrut_13 = Box.createHorizontalStrut(20);
+		contenedorBotones.add(horizontalStrut_13);
+		
+		Component horizontalStrut_14 = Box.createHorizontalStrut(20);
+		contenedorBotones.add(horizontalStrut_14);
+		contenedorCampos.setLayout(new GridLayout(4, 3, 0, 0));
+		
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut_1);
+		
+		JLabel labelNombreUsuario = new JLabel("Nombre de Usuario");
+		labelNombreUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		contenedorCampos.add(labelNombreUsuario);
+		
+		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut_2);
+		
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut);
+		
+		campoNombreUsuario = new JTextField();
+		contenedorCampos.add(campoNombreUsuario);
+		campoNombreUsuario.setColumns(10);
+		
+		Component horizontalStrut_3 = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut_3);
+		
+		Component horizontalStrut_4 = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut_4);
+		
+		JLabel labelPassword = new JLabel("Contrase\u00F1a");
+		labelPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		contenedorCampos.add(labelPassword);
+		
+		Component horizontalStrut_5 = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut_5);
+		
+		Component horizontalStrut_6 = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut_6);
+		
+		campoUsuario = new JTextField();
+		contenedorCampos.add(campoUsuario);
+		campoUsuario.setColumns(10);
+		
+		Component horizontalStrut_7 = Box.createHorizontalStrut(20);
+		contenedorCampos.add(horizontalStrut_7);
+		contenedorLogo.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JLabel labelLogo = new JLabel("");
+		labelLogo.setHorizontalAlignment(SwingConstants.CENTER);
+		labelLogo.setIcon(new ImageIcon(Login.class.getResource("/assets/placeholder.png")));
+		contenedorLogo.add(labelLogo);
+		contenedorPrincipal.setLayout(gl_contenedorPrincipal);
 	}
-
 }
