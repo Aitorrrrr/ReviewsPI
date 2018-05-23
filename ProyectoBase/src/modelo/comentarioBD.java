ee3183d18800a9bf6be2960431b63f509f66582a
Ricardo <ricardas828@gmail.com>
2018-05-23 16:36:50 +0200

    Imprimir los comentarios

--- ProyectoBase/src/modelo/comentarioBD.java
+++ ProyectoBase/src/modelo/comentarioBD.java
@@ -3,15 +3,18 @@ import java.sql.*;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import javax.swing.JTextArea;
+
+import Vista.Pantalla_Usuario;
 
 public class comentarioBD {
 	Calendar fecha=Calendar.getInstance();
 	Calendar calendario = new GregorianCalendar();
-	private static Statement state=null;
+	private static Statement state;
 	
 	public comentarioBD()
 	{
-		
+		this.state=null;
 	}
 	
 	public String crearComentario(String idComentario, Calendar fecha, String hora, String texto, int valoracion, int idUser, int idReview, ConexionBD conbd1)
@@ -30,19 +33,26 @@ public class comentarioBD {
 		return texto;
 	}
 	
-	public String MostrarComentariosUsuario(String idUser, String texto, ConexionBD conbd2)
+	public void MostrarComentariosUsuario(String idUser, ConexionBD conbd2, JTextArea text)
 	{
 		ResultSet rs2;
 		try
 		{
 			state=conbd2.getConexion().createStatement();
-			String sql2="Select Texto from Comentario where idUser=";
+			String sql2="Select Texto from Comentario where idUser='"+idUser+"'";
+			
+			rs2=state.executeQuery(sql2);
+			
+			while (rs2.next())
+			{
+					text.setText(text.getText()+rs2.getString(1)+"\n\n");
+			}
 		}
 		catch (SQLException sql2)
 		{
 			sql2.printStackTrace();
-			return null;
 		}
-		return texto;
 	}
+	
+	
 }
